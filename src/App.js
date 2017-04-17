7481e5d423a3feb9466fb5748d307cd10ca9bb43
Avi Romanoff <avi@romanoff.me>
2017-04-16 21:32:36 -0400

    JoiningPhase instructions

--- src/App.js
+++ src/App.js
@@ -47,7 +47,9 @@ class App extends Component {
     this.state = {
       phaseTime: "5.00",
       timeSincePhaseStart: "2.33234",
-      currentPhase: 'GameInstructions',
+      currentPhase: 'GameJoining',
+      player1: {joined: false},
+      player2: {joined: true},
       location: 'desert',
     }
     // this.onPollTimer()
--- src/phases/JoiningPhase.js
+++ src/phases/JoiningPhase.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-import AvatarMissing from "../images/avatar-missing.svg"
+// import AvatarMissing from "../images/avatar-missing.svg"
+import AvatarMissing from "../images/avatar-missing-with-instructions.svg"
 import AvatarPlayer from "../images/avatar-player.svg"
 
 export default class JoiningPhase extends Component {
@@ -10,9 +11,9 @@ export default class JoiningPhase extends Component {
       <div className="JoiningPhase">
         <div className="JoiningPhase-time">Game begins in {Math.round(Number(this.props.phaseTime) - Number(this.props.timeSincePhaseStart))}</div>
         <div className="JoiningPhase-row">
-          <img src={AvatarPlayer}/>
+          {this.props.player1.joined ? <img src={AvatarPlayer}/> : <img className="JoiningPhase-AvatarMissing" src={AvatarMissing}/>}
           <div className="JoiningPhase-row-vs">VS</div>
-          <img src={AvatarMissing}/>
+          {this.props.player2.joined ? <img src={AvatarPlayer}/> : <img className="JoiningPhase-AvatarMissing" src={AvatarMissing}/>}
         </div>
       </div>
     )
