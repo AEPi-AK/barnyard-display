6ed71151990e29152cfdfa1cc91e856580e4c3af
Avi Romanoff <avi@romanoff.me>
2017-04-16 21:59:22 -0400

    In progress time

--- src/phases/InProgressPhase.js
+++ src/phases/InProgressPhase.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react'
 
+import Clock from "../images/clock.svg"
+
+function timeRemaining({phaseTime, timeSincePhaseStart}) {
+  const secondsRemaining = Math.round(Number(phaseTime) - Number(timeSincePhaseStart))
+  return secondsRemaining < 10 ? `0:0${secondsRemaining}` : secondsRemaining
+}
+
+
 export default class InProgressPhase extends Component {
-  render() { return <div className="Phase-name">InProgressPhase</div> }
+
+  render() {
+    return (
+      <div className="InProgressPhase biome-background">
+        <img className="image-full-background" src={this.props.biomeImages[this.props.location]}/>
+        <div className="Banner">
+          <div className="Banner-title">{this.props.location}</div>
+          <div className="Banner-time">
+            <img src={Clock}/> {timeRemaining(this.props)} remaining
+          </div>
+        </div>
+      </div>
+    )
+  }
+
 }
