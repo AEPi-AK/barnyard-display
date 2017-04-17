7994812df11674686f5c1decccee90a9d4ef5a4b
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-04-17 00:39:36 -0400

    Set up gameScoring phase.

--- src/phases/ScoringPhase.js
+++ src/phases/ScoringPhase.js
@@ -1,5 +1,55 @@
 import React, { Component } from 'react'
+import RainforestImg from "../images/rainforest.jpg"
+
+class Stars extends Component {
+  render() {
+    return(
+      <div className="Stars">
+        star
+      </div>
+    )
+  }
+}
+
+class ResultsPanel extends Component {
+  render() {
+    return(
+      <div className="ResultsPanel-background">
+        <div className="ResultsPanel-header">
+          <div className="ResultsPanel-playerLabel">
+            Player {this.props.playerNum}
+	  </div>
+	  <div className="ResultsPanel-playerScore">
+	    {this.props.score}
+	  </div>
+	</div>
+	<div className="ResultsPanel-imgPanel">
+	  <div className="ResultsPanel-img"/>
+	</div>
+	<div className="ResultsPanel-starPanel">
+          <Stars/>
+          <Stars/>
+          <Stars/>
+	</div>
+        <div className="ResultsPanel-description">
+          Description
+        </div>
+      </div>
+    )
+  }
+}
 
 export default class ScoringPhase extends Component {
-  render() { return <div className="Phase-name">ScoringPhase</div> }
+  render() {
+    return (
+      <div className="ScoringPhase">
+        <img className='image-full-background' src={RainforestImg}/>
+	<div className="ScoringPhase-resultsPanels">
+	  <ResultsPanel playerNum={1} score={5}/>
+	  <ResultsPanel playerNum={2} score={6}/>
+	</div>
+      </div>
+    )
+  }
 }
+
