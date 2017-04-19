162e3903468dd13cd574307ccb3cce92bf1e7f07
Avi Romanoff <avi@romanoff.me>
2017-04-19 15:27:19 -0400

    Fix: other player box shouldn't show during in progress

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
       isLoading: false,
       phaseTime: '5.00',
       timeSincePhaseStart: '2.33234',
-      currentPhase: 'GameScoring',
+      currentPhase: 'GameInProgress',
       player1: {
         slot0: 'Bison',
         slot1: 'TreeFrog',
